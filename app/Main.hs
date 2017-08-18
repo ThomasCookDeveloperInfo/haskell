292e7267bba16d7bacbdfc40fe0702a9d395917c
Thomas Cook <thomas.cook@condecosoftware.com>
2017-08-18 13:27:12 +0100

    more fiddling

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,24 @@
 module Main where
 
 import Lib
+import Data.Typeable
 
 main :: IO()
 main = do
-  putStrLn "What's your name?"
+  let exampleTuple = ("test", True)
+  let exampleTriple = ([1, 2, 3], "test", ['t', 'e', 's', 't'])
+  let exampleList = [1, 2, 3]
+  let exampleListOfList = [[True], [False, False]]
+  let typeOfTuple = typeOf exampleTuple
+
+  putStrLn ("tuple: " ++ (show(exampleTuple)))
+  putStrLn ("triple: " ++(show(exampleTriple)))
+  putStrLn ("head list: " ++ (show(head(exampleList))))
+  putStrLn ("tail list: " ++ (show(tail(exampleList))))
+  putStrLn ("head list of list: " ++ (show(head(exampleListOfList))))
+  putStrLn ("tail of list of list: " ++ (show(tail(exampleListOfList))))
+  putStrLn ("type of tuple: " ++ show(typeOf(exampleTuple)))
+
   name <- getLine
   let greeting = greet name
   putStrLn greeting
