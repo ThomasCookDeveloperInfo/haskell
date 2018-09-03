50e05d8f435a93b350e70861f2f0a0fef130f587
thomascookdeveloperinfo <thomas.cook@condecosoftware.com>
2018-09-03 22:14:38 +0100

    Even more refactoring.

--- app/Main.hs
+++ app/Main.hs
@@ -9,24 +9,10 @@ module Main where
 
   main :: IO()
   main = do
-    print $ "myIsEmpty: " ++ show (myLength "Hello world!")
-    print $ "myIsEmpty: " ++ show (myIsEmpty "Hello world!")
-
-    let roomA = Location 4 "Room A" []
-    let roomB = Location 5 "Room B" []
-
-    let floor1 = Location 1 "Floor 1" [roomA, roomB]
-    let floor2 = Location 2 "Floor 2" [roomA, roomB]
-    let floor3 = Location 3 "Floor 3" [roomA, roomB]
-
-    let workspace = Location 0 "Newcastle Office" [floor1, floor2]
-
-    print $ findWorkspace 5 [workspace]
+    generator <- newStdGen
 
     let expected = 0.0
     let inputs = [0, 1]
-
-    generator <- newStdGen
 
     let inputWeights = randomWeights generator (length inputs) 3
     let hiddenWeights = randomWeights generator 3 1
@@ -34,9 +20,5 @@ module Main where
 
     let outputs = activate inputs [inputWeights, hiddenWeights, outputWeights]
 
-    let c = cost expected (head (toList outputs))
-
     print inputs
     print outputs
-
-    print $ "expected: " ++ (show expected) ++ ", actual: " ++ (show $ head (toList outputs)) ++ ", cost: " ++ (show c)
