86bed5e570eb3b36adaa5b4d357865bb2083e8d4
Thomas <tom.cook@palringo.com>
2020-01-07 16:36:03 +0000

    Make kmeans take s take numClusters and unclustered data and construct that in GUI module

--- src/Gui.hs
+++ src/Gui.hs
@@ -34,7 +34,24 @@ setup window = do
   on UI.click clusterButton $ const $ do
     UI.clearCanvas canvas
 
-    kmeansState <- liftIO (example)
+    let pointA = Point 10 10 None
+    let pointB = Point 30 15 None
+    let pointC = Point 45 30 None
+    let pointD = Point 5 20 None
+    let pointE = Point 45 10 None
+    let pointF = Point 35 15 None
+    let pointG = Point 5 60 None
+
+    let pointA' = Point 100 100 None
+    let pointB' = Point 110 110 None
+    let pointC' = Point 100 110 None
+    let pointD' = Point 110 120 None
+    let pointE' = Point 120 120 None
+    let pointF' = Point 140 130 None
+    let pointG' = Point 50 80 None
+    let unclusteredData = [pointA, pointB, pointC, pointD, pointE, pointF, pointG, pointA', pointB', pointC', pointD', pointE', pointF', pointG']
+
+    kmeansState <- liftIO (kmeans 2 unclusteredData)
 
     forM_ (clusters kmeansState) $ \(Point x y color) -> do
       canvas # set' UI.fillStyle (UI.htmlColor (if color == Black then "black" else if color == Red then "red" else if color == Green then "green" else "blue"))
--- src/Kmeans.hs
+++ src/Kmeans.hs
@@ -2,7 +2,7 @@ module Kmeans(
   KmeansState(..),
   Point(..),
   Color(..),
-  example
+  kmeans
 ) where
 
 import Data.List
@@ -81,33 +81,14 @@ shiftCentroids kmeansState =
         sameColors = filter sameColor (clusters kmeansState)
         sameColor pointA = color pointA == color centroid
 
-kmeans :: [Point]-> [Point] -> KmeansState
-kmeans unclustered initialCentroids = cluster $ KmeansState unclustered initialCentroids where
+kmeans' :: [Point]-> [Point] -> KmeansState
+kmeans' unclustered initialCentroids = cluster $ KmeansState unclustered initialCentroids where
   cluster kmeansState
     | shiftCentroids (assignClusters kmeansState) == kmeansState = kmeansState
     | otherwise = cluster $ shiftCentroids (assignClusters kmeansState)
 
-numClusters = 2
-
-example :: IO KmeansState
-example = do
-
-  let pointA = Point 10 10 None
-  let pointB = Point 30 15 None
-  let pointC = Point 45 30 None
-  let pointD = Point 5 20 None
-  let pointE = Point 45 10 None
-  let pointF = Point 35 15 None
-  let pointG = Point 5 60 None
-
-  let pointA' = Point 100 100 None
-  let pointB' = Point 110 110 None
-  let pointC' = Point 100 110 None
-  let pointD' = Point 110 120 None
-  let pointE' = Point 120 120 None
-  let pointF' = Point 140 130 None
-  let pointG' = Point 50 80 None
-  let unclusteredData = [pointA, pointB, pointC, pointD, pointE, pointF, pointG, pointA', pointB', pointC', pointD', pointE', pointF', pointG']
+kmeans :: Int -> [Point] -> IO KmeansState
+kmeans numClusters unclusteredData = do
 
   let minWidth = minimum $ map x unclusteredData
   let maxWidth = maximum $ map x unclusteredData
@@ -119,9 +100,5 @@ example = do
   let randomPoints = map (unclusteredData!!) (take numClusters $ randomRs (0, length unclusteredData) randGen)
 
   let initialCentroids = zipWith (curry (\(index, Point x y _) -> Point x y $ toEnum index)) [1..] randomPoints
-
-  putStrLn $ "Initial centroids: " ++ show initialCentroids
-
-  putStrLn $ "Unclustered data: " ++ show unclusteredData
 
   return (assignClusters $ KmeansState unclusteredData initialCentroids)
